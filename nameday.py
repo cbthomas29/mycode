eda001ba67817f48da4c66a5977a0266f01bc147
cbthomas29 <cbthomas29@gmail.com>
2024-02-06 19:07:30 +0000

    join and split

---
+++ nameday.py
@@ -0,0 +1,9 @@
+
+# Input for asking users name
+name = input('What is your name?')
+
+# Input asking for the day of the week
+day_of_week = input('What day of the week is it?')
+
+# Print Statement
+print(f'hello, {name}! Happy {day_of_week}!')
